30feaff187fbdbea6fc3dca114dde2d531082219
brentkuz <brentkuzmanich@gmail.com>
2016-07-02 16:52:03 -0700

    change comment

--- JS-Tools/object-mapper.js
+++ JS-Tools/object-mapper.js
@@ -4,7 +4,7 @@
  * Comment: A simple, functional approach to mapping objects.
  *
  * Sample Usage:
- * my.objectMapper.createMap('obj1_obj2', function(from, to){
+ * my.objectMapper.createMap('obj1->obj2', function(from, to){
  *      to.prop1 = from.prop1;
  *      to.prop2 = from.prop2;
  * };
@@ -13,7 +13,7 @@
  * var someObj = { prop1: 'asdf', prop2: 123 };  
  * var anotherObj = { prop1: '', prop2: '', aMethod: function(){ alert(this.prop1); } };
  *
- * my.objectMapper.map('obj1_obj2', someObj, anotherObj);
+ * my.objectMapper.map('obj1->obj2', someObj, anotherObj);
  *
  */
 
